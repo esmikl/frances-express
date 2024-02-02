d0798942bad6ba118ded40c96bdd9301bcb19ee9
Emily Mikl <emilysmikl@gmail.com>
2024-02-01 19:45:05 -0600

    Main page

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  encapsulation: ViewEncapsulation.None
+})
+export class HomeComponent {
+
+}
